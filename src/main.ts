f63768e23e2636d34cd621cc3c2fc7c27287d364
omkark958 <omkarkulkarni958@gmail.com>
2025-06-01 00:18:00 +0530

    main.ts cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+//   app.enableCors({
+//   origin: ['http://localhost:4200', 'https://your-flutter-web-app.web.app'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+// });
+app.enableCors({
+  origin: true, // reflects request origin
+  credentials: true,
+});
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
