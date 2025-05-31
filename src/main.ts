e91fbe7a3a2641199ed584e7e3f8d805f7bb4552
omkark958 <omkarkulkarni958@gmail.com>
2025-06-01 00:20:54 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,25 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-//   app.enableCors({
-//   origin: ['http://localhost:4200', 'https://your-flutter-web-app.web.app'],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true,
-// });
-app.enableCors({
-  origin: true, // reflects request origin
-  credentials: true,
-});
+  //   app.enableCors({
+  //   origin: ['http://localhost:4200', 'https://your-flutter-web-app.web.app'],
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  //   credentials: true,
+  // });
+  app.enableCors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: (origin, callback) => {
+      if (!origin || origin.startsWith('http://localhost') || origin.startsWith('https://omkarpkulkarni.netlify.app')) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  });
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
