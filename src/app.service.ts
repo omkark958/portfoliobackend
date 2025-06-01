3cb2ad2280b891af791b2d06afe3fa3e6bdff482
omkark958 <omkarkulkarni958@gmail.com>
2025-06-01 11:21:37 +0530

    lets go

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! again may be';
+    return 'Hello World! again may be 😂👍';
   }
 }
